707c9cd84766cd30d8288467daccbb3b6c9caf15
Hoto Ras <jdh59681@gmail.com>
2024-01-08 11:09:02 +0900

    New csproj
    yup

---
+++ hello_world/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace hello_world;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string man = "아저씨"; // string === System.String
+        Console.WriteLine("Hello, World!\n{0}", man);
+    }
+}
