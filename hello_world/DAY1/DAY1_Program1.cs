0aa90936b641d6305105c44c53ad316c1def5e6b
Hoto Ras <jdh59681@gmail.com>
2024-01-08 14:06:07 +0900

    주석이나 넣어보자

--- hello_world/DAY1/DAY1_Program1.cs
+++ hello_world/DAY1/DAY1_Program1.cs
@@ -4,7 +4,7 @@ namespace hello_world1
     /*public*/
     class DAY1_Program1
     {
-        public DAY1_Program1(string[] args)
+        public DAY1_Program1()
         {
             string man = "아저씨1";
             Console.WriteLine("Hello, World!");
@@ -17,12 +17,16 @@ namespace hello_world2
 {
     class DAY1_Program1
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// 뷁
+        /// </summary>
+        /// <param name="args">main의 그거</param>
+        public DAY1_Program1(string[] args)
         {
             string man = "아저씨2";
             Console.WriteLine("Hello, World2!");
             Console.WriteLine(man);
-            var test = new hello_world1.DAY1_Program1(args);
+            var test = new hello_world1.DAY1_Program1();
         }
     }
 }
--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace hello_world;
+﻿using hello_world2;
+
+namespace hello_world;
 
 class Program
 {
-    static void _Program(string[] args)
+    static void Main(string[] args)
     {
         string man = "아저씨"; // string === System.String
         Console.WriteLine("Hello, World!\n{0}", man);
+        var temp = new hello_world2.DAY1_Program1(args);
     }
 }
