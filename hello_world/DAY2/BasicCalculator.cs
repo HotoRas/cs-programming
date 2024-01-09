05fde01b9bc0d9f92f26be2d91ebc86056f67b39
Hoto Ras <jdh59681@gmail.com>
2024-01-09 11:12:48 +0900

    calculators basics

---
+++ hello_world/DAY2/BasicCalculator.cs
@@ -0,0 +1,36 @@
+namespace hello_world;
+
+public class Day1_Program11
+{
+    static void Main() { BasicCalculator(); }
+    public static void BasicCalculator()
+    {
+        int
+        x = 10,
+        y = 3;
+        double
+        x2 = 10,
+        y2 = 3.14;
+
+        int
+        result1 = x + y,
+        result2 = x - y,
+        result3 = x * y;
+        double
+        result4 = x / y;
+
+        double
+        result5 = y / x,
+        result6 = x % y,
+        result7 = y % x;
+
+        Console.WriteLine("a와 b의 연산 결과 (정수):");
+        Console.WriteLine($"sum: {result1}, sub: {result2}, mul: {result3}, sub: {result4} {result5}, div: {result6} {result7}");
+
+        Console.WriteLine("a와 b의 연산 결과 (실수):");
+        Console.WriteLine($"sub: {x2 / y2}, div: {x % y}");
+
+        Console.WriteLine("a와 b의 연산 결과 (정수, 실수):");
+        Console.WriteLine($"mul: {x * y2}");
+    }
+}
