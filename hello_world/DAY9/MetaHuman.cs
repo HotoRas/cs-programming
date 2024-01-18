85da7e2852c197ac4eaf0993ebd538f3e108272d
Hoto Ras <jdh59681@gmail.com>
2024-01-18 13:35:06 +0900

    Meta Human example

---
+++ hello_world/DAY9/MetaHuman.cs
@@ -0,0 +1,22 @@
+namespace csproject;
+public class Day4_Program8_Exam2{
+    static void Run(){
+        SuperMan superman = new("Superman", 30, 4625, "fly; laser;");
+    }
+}
+public class MetaHuman{
+    public string Name;
+    protected string privName;
+    protected int Age;
+    protected int Power;
+    protected string Skill;
+}
+public class SuperMan: MetaHuman{
+    public SuperMan(string name, int age, int power, string skill){
+        privName = "Klerk Kent";
+        Name = name;
+        Age = age;
+        Power = power;
+        Skill = skill;
+    }
+}
